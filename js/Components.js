ab317ecdfc37bdc6ac361bd9dd97cff93b027655
jhonny-m <jhonnyporto@hotmail.com>
2017-03-09 21:50:26 +0000

    More Components added

--- js/Components.js
+++ js/Components.js
@@ -8,8 +8,12 @@ var BaseEnergyLossRate= 0.01;
 var BaseHungerGainRate= 0.5;
 var BaseEnergyGainRate= 0.1;
 var StartingMoney = 100;
+var DayLenght = 20*1000;
+var BaseSongLenght = 5000;
+var StatLevelSongLenght = 500;
 
-Position = function (positionX,positionY){
+
+Position = function(positionX,positionY){
 	var self = {};
 	self.x= positionX;
 	self.y= positionY;
@@ -17,6 +21,23 @@ Position = function (positionX,positionY){
 		self.x += moveX;
 		self.y += moveY;
 	}
+	return self;
+}
+
+ObjectSprite = function(spritePositionX,spritePositionY,sizeX,sizeY,sprite){
+	var self = {};
+	self.spritePosition=Position(spritePositionX,spritePositionY);
+	self.size = Position(sizeX,sizeY);
+	self.sprite = sprite;
+	
+	self.draw = function(position){
+		ctx.drawImage(self.sprite,self.spritePosition.x,self.spritePosition.y,self.size.x,self.size.y,position.x,position.y)
+	}
+	
+	self.changeSpritePosition = function(){
+		
+	}
+	
 	return self;
 }
 
@@ -91,5 +112,98 @@ HealthState = function(name){
 
 Clock = function(){
 	var self = {};
+	self.day = 1;
+	self.hours =0;
+	self.percentage = 0.0;
+	self.dayLenght = DayLenght;
+	self.updatePercentage = function(){
+		self.percentage = self.hours/ self.dayLenght;
+	}
 	
+	self.update = function(time){
+		self.hours += time;
+		while(true){
+			if (self.hours >= self.dayLenght){
+				self.day++;
+				self.hours -= self.dayLenght;
+			}
+			else
+				break;
+		}
+		self.updadePercentage();
+	}
 }
+
+Song = function(name,statLevel){
+	var self = {};
+	self.name = name;
+	self.progress =0;
+	self.practice =0;
+	self.maxProgress =BaseSongLenght + StatLevelSongLenght * statLevel;
+	
+	self.updateWritting = function(time){
+		self.progress += time;
+		if (self.progress>self.maxProgress){
+			self.progress = self.maxProgress;
+		}
+ 	}
+	
+	self.updatePractice = function(time){
+		self.practice += time;
+		if (self.practice>self.maxProgress){
+			self.practice = self.maxProgress;
+		}
+ 	}
+	
+	self.isFullyWritten = function(){
+		if( self.progress >= self.maxProgress)
+				return true;
+		else
+			return false;
+	}
+	
+	self.isFullyPracticed = function(){
+		if( self.practice >= self.maxProgress)
+				return true;
+		else
+			return false;
+	}
+	return self;
+}
+
+SongBook = function(){
+	var self = {};
+	self.songs = [];
+	self.name = "Song number "
+	self.songsWritten = 0;
+	self.songsPracticed = 0;
+	self.writeSong = function(level,time){
+		if(self.songsWritten==self.songs.length){
+			self.newSong(level);
+		}
+		self.songs[self.songsWritten].updateWritting(time);
+		if(self.songs[self.songsWritten].isFullyWritten){
+			self.songsWritten++;
+		}
+	}
+	
+	self.newSong = function(level){
+		self.songs[self.songsWritten]= Song(self.name + self.songsWritten, level);
+	}
+	self.practiceSong = function(time){
+		if(self.songsWritten>self.songsPracticed){
+			self.songs[self.songsPracticed].updatePractice(time);
+			if(self.songs[self.songsPracticed].isFullyPracticed())
+				self.songsPracticed++;
+		}
+	}
+	
+	return self;
+}
+
+// lala = SongBook();
+// lala.writeSong(1,10000);
+// lala.writeSong(1,10000);
+// lala.practiceSong(10000);
+// lala.practiceSong(10000);
+// console.log(lala.songs);
